75108b7ef46b70ee65e4a75cbaabbc026cb4190c
khurram-hassan <68346553+khurram-hassan@users.noreply.github.com>
2022-11-01 19:52:21 +0530

    setup script to run from my personal repo

--- setup.ps1
+++ setup.ps1
@@ -1,18 +1,18 @@
 #If the file does not exist, create it.
 if (-not(Test-Path -Path $PROFILE -PathType Leaf)) {
      try {
-         Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-         Write-Host "The profile @ [$PROFILE] has been created."
+        Invoke-RestMethod https://raw.githubusercontent.com/khurram-hassan/powershell-profile/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+        Write-Host "The profile @ [$PROFILE] has been created."
      }
      catch {
-         throw $_.Exception.Message
+        throw $_.Exception.Message 
      }
  }
 # If the file already exists, show the message and do nothing.
  else {
-		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
-		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+    Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
+    Invoke-RestMethod https://raw.githubusercontent.com/khurram-hassan/powershell-profile/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+    Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
  }
 & $profile
 
